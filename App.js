883026252f603e27b7d74ecff5a9275fc1e15e29
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-05-07 19:09:58 -0300

    wheather.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './screen/Weather';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Weather></Weather>
       <StatusBar style="auto" />
     </View>
   );
---
+++ screen/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Weather = () => { 
+    return(
+        <View>
+            <Text>Estufa Inteligente APP</Text>
+        </View>
+    )
+} 
+
+export default Weather
+const style = StyleSheet.create({})
