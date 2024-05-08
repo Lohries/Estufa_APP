f40f5cf85c98407ff360c5d33293d860f0f79606
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-05-07 21:16:43 -0300

    backgrounds

--- screen/Weather.js
+++ screen/Weather.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import background from "../assets/back4.jpeg";
 
 const Weather = () => { 
     return(
-        <View>
-            <Text>Estufa Inteligente APP</Text>
-        </View>
+        <ImageBackground source={background} style={styles.container}>
+            <View style={styles.temperature}>
+                <Text style={styles.text}>Temperature</Text>
+            </View>
+        </ImageBackground>
     )
 } 
 
-export default Weather
-const style = StyleSheet.create({})
+export default Weather;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        resizeMode: "contain",
+        justifyContent: "center",
+        width: "100%", 
+        height: "100%", 
+    },
+    temperature: {
+        flex: 1, 
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 25,
+        textAlign: "center",
+        color: "gray",
+        fontWeight: "150",
+    }
+});
