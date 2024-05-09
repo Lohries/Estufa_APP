1b0f44cb73e4984fd923134e5a8860de23ea3857
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-05-09 11:08:08 -0300

    home page done

--- screen/Weather.js
+++ screen/Weather.js
@@ -6,8 +6,23 @@ const Weather = () => {
     return(
         <ImageBackground source={background} style={styles.container}>
             <View style={styles.temperature}>
-                <Text style={styles.text}>Temperature</Text>
+                <Text style={styles.text}>75°C</Text>
             </View>
+            <View style={styles.data}>
+                <View style={styles.spacer}></View>
+                <View style={styles.dataWrapper}>
+                    <View style={styles.humidity}>
+                        <Text style={styles.dataText}>23%</Text>
+                        <Text style={styles.title}>Humidity</Text>
+                    </View>
+                    <View style={styles.pressure}>
+                        <Text style={styles.dataText}>0.5 ATM</Text>
+                        <Text style={styles.title}>Pressure</Text>
+                    </View>
+                </View>
+
+            </View>
+
         </ImageBackground>
     )
 } 
@@ -27,9 +42,61 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     text: {
-        fontSize: 25,
+        fontSize: 150,
         textAlign: "center",
-        color: "gray",
-        fontWeight: "150",
-    }
+        color: "black",
+        fontWeight: "100",
+        paddingRight: 50
+    },
+    data: {
+        flex: 1,
+        textAlign: "center",
+        justifyContent: "center",
+        width: "100%",
+    
+  
+    },
+    spacer: {
+        height: "30%"
+    },
+    dataWrapper:  {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "80%",
+        height: "20%",
+        backgroundColor: "rgba(169, 227, 163, 1)",
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: "white",
+        marginLeft: 50
+    },
+    humidity:  {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    pressure: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    dataText: {
+        fontSize: 20,
+        textAlign: "center",
+        fontFamily: "Helvetica",
+        color: "white",
+        fontWeight: "200"
+
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center",
+        fontFamily: "Helvetica"
+
+    },
 });
